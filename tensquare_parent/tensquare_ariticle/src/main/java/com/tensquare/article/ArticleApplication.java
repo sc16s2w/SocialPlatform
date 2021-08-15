d25af922ec4d5834c685cdf3c7405dca4c2990ac
sc16s2w <765012395@qq.com>
2021-08-16 00:31:44 +0200

    eureka and rsa service finish

--- tensquare_parent/tensquare_ariticle/src/main/java/com/tensquare/article/ArticleApplication.java
+++ tensquare_parent/tensquare_ariticle/src/main/java/com/tensquare/article/ArticleApplication.java
@@ -3,19 +3,16 @@ package com.tensquare.article;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-=======
+
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
->>>>>>> eureka and rsa service finish
+
 import org.springframework.context.annotation.Bean;
 import util.IdWorker;
 
 @SpringBootApplication
 @MapperScan("com.tensquare.article.dao")
-<<<<<<< HEAD
-=======
 @EnableEurekaClient
->>>>>>> eureka and rsa service finish
+
 public class ArticleApplication {
     public static void main(String[] args){
         SpringApplication.run(ArticleApplication.class,args);
